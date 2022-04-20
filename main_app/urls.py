7b1354e3b65ee3b90a279aa1d67330cd3b9c8243
neshmasood <neshtam@gmail.com>
2022-04-19 23:38:13 -0500

    complete login/logout view, urls and template page. login working properly

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
             
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignUpForm
 
 # Create your views here.
@@ -9,6 +11,37 @@ from .forms import SignUpForm
 class HomePage(TemplateView): 
     template_name = 'home.html' 
 
+
+# django auth
+
+def login_view(request):
+    # if POST, then authenticate the user (submitting the username and password)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    return render(request, 'login.html', {'form': form})
+            else:
+                return render(request, 'login.html', {'form': form})
+        else: 
+            return render(request, 'signup.html', {'form': form})
+    else: 
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 
 
 def signup_view(request):
@@ -20,7 +53,7 @@ def signup_view(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('/')
+            return redirect('/login/')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
