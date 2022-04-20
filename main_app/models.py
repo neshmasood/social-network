193546dcacfe57ad70fba6825f4b6c43d04b8bc4
neshmasood <neshtam@gmail.com>
2022-04-20 00:28:39 -0500

    create post model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django .urls import reverse
+from datetime import datetime, date
+
 
 # Create your models here.
+
+
+class Post (models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(null = True, blank = True, upload_to="images/")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+    
+    
+    
+    def __str__(self):
+        return self.title + '|' + str(self.author)
+    
+    def get_absolute_url(self):
+        return reverse('home')
