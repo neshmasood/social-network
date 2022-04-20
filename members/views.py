dc4cbe07211462fd921759b8fd0c4593a2eb7422
neshmasood <neshtam@gmail.com>
2022-04-19 22:50:08 -0500

    create user regsitration, template and forms. registering user is working properly.

--- members/views.py
+++ members/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views import generic
+from .forms import SignUpForm
+
+
 
 # Create your views here.
+
+class UserRegisterView(generic.CreateView):
+    form_class= SignUpForm
+    template_name = 'registration/register.html'
+    
+
