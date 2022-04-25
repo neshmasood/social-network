361fd6d63157cb66ac43fb042b5fd6b24b323455
neshmasood <neshtam@gmail.com>
2022-04-25 14:43:00 -0500

    update views.py in payments, create stripe checkout session and success and cancel views

--- payments/views.py
+++ payments/views.py
@@ -1,16 +1,42 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django.conf import settings
-from django.urls import reverse
-from django.http import JsonResponse
+# from django.urls import reverse
+# from django.http import JsonResponse
+from django.views import View
 import stripe
+from main_app.models import Price, Product
 
 
-stripe.api_key = settings.STRIPE_PUBLISHABLE_KEY
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your views here.
 
+class CreateCheckoutSessionView(View):
+    
+    def post(self, request, *args, **kwargs):
+        price = Price.objects.get(id=self.kwargs['pk'])
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price': price.stripe_price_id,
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=settings.BASE_URL + '/payments/success/',
+            cancel_url=settings.BASE_URL + '/payments/cancel/',
+        )
+        return redirect(checkout_session.url)
 
+
+class SuccessView(TemplateView):
+    template_name = "success.html"
+
+
+class CancelView(TemplateView):
+    template_name = "cancel.html"
 
 
 class DonationPage(TemplateView):
@@ -18,8 +44,13 @@ class DonationPage(TemplateView):
     
     
     def get_context_data(self, *args, **kwargs):
-        context= super().get_context_data(*args, **kwargs)
-        context['key'] = settings.STRIPE_PUBLISHABLE_KEY
+        product = Product.objects.get(name="Donation")
+        prices = Price.objects.filter(product=product)
+        context = super(DonationPage, self).get_context_data(*args, **kwargs)
+        context.update({
+            "product": product,
+            "prices": prices
+        })
         return context 
     
     
